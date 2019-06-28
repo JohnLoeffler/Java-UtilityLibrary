bdaf3705c98df5c1bc941d4ede5d63ba15f26d76
John Loeffler <John.Loeffler@gmail.com>
2019-06-28 06:09:50 -0400

    Update Clicker.java

--- UtilityLibrary/src/com/loeffler/utilitylibrary/Tools/Clicker.java
+++ UtilityLibrary/src/com/loeffler/utilitylibrary/Tools/Clicker.java
@@ -1,21 +1,20 @@
-/*
- * 
- * 
- * 
- */
-
 package com.loeffler.utilitylibrary.Tools;
 
 /**
  *  <p><strong>Clicker</strong></p>
  *  <em>@author</em>  John Loeffler
- *  
+ *    
  *  <strong>Contact</strong> 
  *    <em>@Email</em>     John.Loeffler@gmail.com
  *    <em>@Twitter</em>   @ThisDotJohn
  *    <em>@LinkedIn</em>  LinkedIn.com/in/JohnLoeffler
  *    <em>@Github</em>    github.com/JohnLoeffler
  *    <em>@Website</em>   JohnLoeffler.com
+ *
+ *
+ *  A new tool I'm messing around with. We all use mod alot to trigger a sequence of some kind
+ *    and sometimes I'd really love to just have a button to press every time instead. A work
+ *    in progress, but its function, if not all that helpful yet.
  */
 public class Clicker {
   private int Tick, Mod;
